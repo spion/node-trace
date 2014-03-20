66fe5beeafd93d12b71d4bc875cc77097e2c3131
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-03-20 15:47:05 +0100

    fixed things, renamed to node-trace

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-trace
+
+TODO: write readme
--- bin/main.js
+++ bin/main.js
@@ -1,19 +1,28 @@
+#!/usr/bin/env node
+
+require.extensions['.js'] = loadModule;
+
 var fs         = require('fs');
 var argv       = require('optimist').argv;
 var instrument = require('../index')
 var resolve    = require('resolve');
 
 if (argv.tracer)
-    var tracer = require(resolve(process.cwd(), argv.tracer));
+    var tracer = require(resolve.sync(argv.tracer, {basedir: process.cwd()}));
 else
-    var tracer = require('./default-tracer');
+    var tracer = require('../default-tracer');
 
 global.$$__trace = tracer.trace;
 
+
 function loadModule(module, filename) {
+    if (!loadModule.tracingEnabled)
+        return module._compile(fs.readFileSync(filename, 'utf8'), filename);
     var instrumented = transformModule(filename, tracer.filter);
     module._compile(instrumented, filename);
 }
+
+loadModule.tracingEnabled = false;
 
 function always() { return true; }
 
@@ -21,9 +30,17 @@ function transformModule(name, fileFilter) {
     fileFilter = fileFilter || always;
     var src = fs.readFileSync(name, 'utf8')
     if (!fileFilter(name)) return src;
-    else return instrument(src);
+    else try {
+        return instrument(src);
+    } catch (e) {
+        console.error("Error instrumenting", name)
+        throw e;
+    }
 }
 
 
-require.extensions['.js'] = loadModule;
-require(argv._[0]);
+var rmod = resolve.sync(argv._[0], {basedir: process.cwd()});
+
+loadModule.tracingEnabled = true;
+process.argv = argv._;
+require(rmod);
